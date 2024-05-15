62abdbe536584dbcb9153cfda18b208489ca501d
ValeraDanilov <delfiyets@gmail.com>
2024-05-15 22:02:48 +0300

    HQL, JPQL, Native SQL [#505064]

--- src/main/java/ru/job4j/social_media_api/repository/PostRepository.java
+++ src/main/java/ru/job4j/social_media_api/repository/PostRepository.java
@@ -2,8 +2,12 @@ package ru.job4j.social_media_api.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.social_media_api.model.Post;
+import ru.job4j.social_media_api.model.User;
 
 
 import java.time.LocalDateTime;
@@ -16,4 +20,38 @@ public interface PostRepository extends CrudRepository<Post, Integer> {
     List<Post> findAllByCreatedBetween(LocalDateTime startDate, LocalDateTime finishDate);
 
     Page<Post> findAllByOrderByCreatedDesc(Pageable pageable);
+
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            """
+                    update Post post set post.title = :title, post.description = :description
+                    where post.id = :id
+                    """
+    )
+    void updatePostTitleAndDescription(@Param("title") String title, @Param("description") String description, @Param("id") int id);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            """
+                    update Post post set post.photo = null where post.id = :id
+                    """
+    )
+    void deletePhotoFromPost(@Param("id") int id);
+
+    @Query(
+            """
+                    delete from Post post where post.id = :id
+                    """
+    )
+    void deletePost(@Param("id") int id);
+
+
+    @Query(
+            """
+                    select post from Post post
+                    join FriendRequest  fr on fr.senderId = :senderId
+                    where fr.receiverId = post.user.id and fr.status = false order by post.created desc
+                    """)
+    Page<Post> findAllPostFromUserSubscriptions(@Param("senderId") int senderId, Pageable pageable);
 }
--- src/main/java/ru/job4j/social_media_api/repository/UserRepository.java
+++ src/main/java/ru/job4j/social_media_api/repository/UserRepository.java
@@ -1,8 +1,39 @@
 package ru.job4j.social_media_api.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.social_media_api.model.User;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface UserRepository extends CrudRepository<User, Integer> {
+    @Query(
+            """
+                    select user from User user
+                    where user.username = :username and user.password = :password
+                    """
+    )
+    Optional<User> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+
+
+    @Query(
+            """
+                    select user from User user
+                    join FriendRequest fr on user.id = fr.senderId
+                    where fr.receiverId = :receiverId and fr.status = false
+                    """
+    )
+    List<User> findAllSubscribers(@Param("receiverId") int receiverId);
+
+    @Query(
+            """
+                    SELECT CASE WHEN fr.senderId = :friendId
+                    THEN fr.receiverId ELSE fr.senderId END
+                    FROM FriendRequest fr
+                    WHERE (:friendId = fr.senderId OR :friendId = fr.receiverId) AND fr.status = true
+                    """
+    )
+    List<User> findAllFriends(@Param("friendId") int friendId);
 }
